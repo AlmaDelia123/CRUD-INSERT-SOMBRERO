d0e6f489ef7fdebaa8bbe05c010d9696fda7125e
Jared-00 <L20070010@huetamo.tecnm.mx>
2023-12-06 08:25:17 -0600

    x1

--- config/conexion.js
+++ config/conexion.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const defaultConfig = {
     host: 'localhost',
     user: 'root',
-    password: 'root2023',
+    password: 'itsh2023',
     database: 'sombreros_calentanos'
 };
 
